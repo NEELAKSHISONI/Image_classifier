27f397d2f8b8b0883ca5f583ea50f013f7e095fa
NEELAKSHISONI <113483146+NEELAKSHISONI@users.noreply.github.com>
2023-09-08 10:28:15 -0700

    change when whole program ran

--- classifier.py
+++ classifier.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 from flask import Flask, request, jsonify
 import numpy as np
@@ -8,19 +9,43 @@ base_model = ResNet50(weights='imagenet')
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Welcome to the ResNet50 predictor. Send a POST request with an image to /predict to get predictions."
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the image from the POST request
-    image = tf.image.decode_image(request.files.get('file').read(), channels=3)
-    image = tf.image.resize(image, [224, 224])
-    
-    # Preprocess & predict
-    image_array = np.expand_dims(image, axis=0)
-    image_array = preprocess_input(image_array)
-    predictions = base_model.predict(image_array)
-    decoded_predictions = decode_predictions(predictions, top=5)[0]
-    
-    return jsonify(decoded_predictions)
+    try:
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file provided'}), 400
+
+        # Get the image from the POST request
+        image = tf.image.decode_image(request.files.get('file').read(), channels=3)
+        image = tf.image.resize(image, [224, 224])
+        
+        # Preprocess & predict
+        image_array = np.array(np.expand_dims(image.numpy(), axis=0))
+        image_array = preprocess_input(image_array)
+        predictions = base_model.predict(image_array)
+        decoded_predictions = decode_predictions(predictions, top=5)[0]
+        
+        # Convert decoded_predictions to a serializable format
+        serializable_predictions = []
+        for label, name, score in decoded_predictions:
+            serializable_predictions.append({
+                "label": label,
+                "name": name,
+                "score": float(score)  # Convert float32 to native Python float
+            })
+        
+        return jsonify(serializable_predictions)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
